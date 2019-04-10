a5c7827022c78c77eb7317d14601295d557e501a
ajdinl <ajdinl>
2019-04-10 11:16:59 +0200

    bobanu na check

--- app.js
+++ app.js
@@ -5,6 +5,32 @@ const express = require('express')
 
 mongoose.connect('mongodb://ajdin:qqqq1111@ds135726.mlab.com:35726/engineer', { useNewUrlParser: true })
 
+const appSchema = new mongoose.Schema({
+    _id: ObjectId,
+    type: String,
+    name: String,
+    createdAt: Date,
+    deleted: Boolean,
+    enabled: Boolean,
+    price: Number,
+    meta: {
+    package: String,
+    platform: String,
+    }
+  })
+const pointsSchema = new mongoose.Schema ({
+    _id: ObjectId,
+    applicationId: String,
+    points: Number,
+    status: String,
+    updatedAt: Date,
+    createdAt: Date,
+    completedAt : Date
+  })
+
+const Applications = mongoose.model('applications', appSchema)
+const Points = mongoose.model('points', pointsSchema )
+
 app.get('/', (req,res) => res.send('Hello !!'))
 
 app.listen(3000, () => console.log('Server ready!!!'))
