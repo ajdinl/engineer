847a4b30bcbe067befd039340a12f675cd402dda
ajdinl <ajdinl>
2019-05-22 20:10:55 +0200

    id na pola gotov

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require('express')
 mongoose.connect('mongodb://ajdin:qqqq1111@ds135726.mlab.com:35726/engineer', { useNewUrlParser: true })
 
 const appSchema = new mongoose.Schema({
+    _id: String,
     type: String,
     name: String,
     createdAt: Date,
@@ -18,6 +19,7 @@ const appSchema = new mongoose.Schema({
     }
   })
 const pointsSchema = new mongoose.Schema ({
+    _id : String,
     applicationId: String,
     points: Number,
     status: String,
@@ -29,45 +31,37 @@ const pointsSchema = new mongoose.Schema ({
 const Applications = mongoose.model('application', appSchema)
 const Points = mongoose.model('point', pointsSchema)
 
-// app.get('/api/application', async (req,res) => {
-//   const [applications, points] = await Promise.all([
-//     Applications.find({}),
-//     Points.find({ status: 'complete'})
-//   ])
-//   const appFilter = applications.map(x => ({applicationId: x.id, name: x.name,platform: x.meta.platform}))
-//   const grouped = Array.from(points
-//     .filter(x => x.points)
-//     .reduce((m, { applicationId, points }) => m.set(applicationId, (m.get(applicationId) || 0) + points), new Map),
-//     ([applicationId, points]) => ({ applicationId, points })
-//   )
-//   const resultFilter = []
-//   appFilter.forEach((itm, i) => {
-//     resultFilter.push({...itm, ...grouped[i]})
-//   })
-//   res.json(resultFilter)
-// })
-
-// app.get('/api/application/:id' , async (req,res) => {
-// const { id } = req.params
-//   // let responseFromDb
-//   // Applications.findOne({_id: id}, (err, result) => {
-//   //   console.log(err, result, 'jebo mater')
-//   //   responseFromDb = {idi: req.params}
-//   //   console.log(responseFromDb)
-//       Applications.findOne({_id: '59a9109c0801d6ad4b2b81b6'}, (err, result) => {
-//         console.log(err, result, 'jebo mater')
-//       })
-//   res.json(id)
-//   })
-
-  // nemoj gledat taj gornji dio nikako za sad nam ne trebaju rute i to
-  // app.listen(3000, () => console.log('Server ready!!!'))
-
-  // Applications.find({}, (err, result) => {
-  //   console.log(err, result, 'sve')
-  // })
-
-  Applications.find({}, (err, result) => {
-    console.log(err, result, 'po ovom sranju ga nadje')
- 
+app.get('/api/application', async (req,res) => {
+  const [applications, points] = await Promise.all([
+    Applications.find({}),
+    Points.find({ status: 'complete'})
+  ])
+  const appFilter = applications.map(x => ({applicationId: x.id, name: x.name,platform: x.meta.platform}))
+  const grouped = Array.from(points
+    .filter(x => x.points)
+    .reduce((m, { applicationId, points }) => m.set(applicationId, (m.get(applicationId) || 0) + points), new Map),
+    ([applicationId, points]) => ({ applicationId, points })
+  )
+  const resultFilter = []
+  appFilter.forEach((itm, i) => {
+    resultFilter.push({...itm, ...grouped[i]})
   })
+  res.json(resultFilter)
+})
+
+app.get('/api/application/:id' ,  (req,res) => {
+  const {id, name} = req.params
+  let responseFromDb
+  Applications.findById(id, 'name meta.platform' , (err, result) => {
+    if(err){
+      console.log(err)
+    } else {
+      result
+      console.log(result)
+    }
+    responseFromDb = {result}
+    res.json(responseFromDb)
+  })
+})
+
+app.listen(3000, () => console.log('Server ready!!!'))
